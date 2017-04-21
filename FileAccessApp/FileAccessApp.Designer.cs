fe341a91566adef593c62bdf4c91b2a909828be3
Spinal7 <ernso7@gmail.com>
2017-04-21 15:15:54 -0400

    File Added Functionality Added for button

---
+++ FileAccessApp/FileAccessApp.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FileAccessApp
+{
+    partial class FrmFileAccess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lstBoxItmsToDisp = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(140, 37);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(156, 17);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Name stored in file:";
+            // 
+            // lstBoxItmsToDisp
+            // 
+            this.lstBoxItmsToDisp.FormattingEnabled = true;
+            this.lstBoxItmsToDisp.ItemHeight = 17;
+            this.lstBoxItmsToDisp.Location = new System.Drawing.Point(101, 72);
+            this.lstBoxItmsToDisp.Name = "lstBoxItmsToDisp";
+            this.lstBoxItmsToDisp.Size = new System.Drawing.Size(224, 140);
+            this.lstBoxItmsToDisp.Sorted = true;
+            this.lstBoxItmsToDisp.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(175, 233);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(52, 292);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(0, 17);
+            this.lblWarning.TabIndex = 3;
+            // 
+            // FrmFileAccess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(436, 357);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lstBoxItmsToDisp);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Lucida Fax", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmFileAccess";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                                              File Access App";
+            this.Load += new System.EventHandler(this.FrmFileAccess_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ListBox lstBoxItmsToDisp;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ FileAccessApp/FileAccessApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileAccessApp
+{
+    public partial class FrmFileAccess : Form
+    {
+        public FrmFileAccess()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFileAccess_Load(object sender, EventArgs e)
+        {
+            lstBoxItmsToDisp.Text = "";
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lstBoxItmsToDisp.Items.Clear();
+            string inValue;
+
+            try
+            {
+                using (StreamReader input = new StreamReader("name.txt"))
+                {
+                    while ((inValue = input.ReadLine()) != null)
+                    {
+                        //Add text read from the file to the listBox object on the form
+                        lstBoxItmsToDisp.Items.Add(inValue);
+                    }
+                }
+
+                lblWarning.ForeColor = Color.Purple;
+                lblWarning.Text = "File read Successfully!";
+            }
+            catch(FileNotFoundException exc)
+            {
+                lblWarning.ForeColor = Color.Red;
+                lblWarning.Text = exc.Message;
+            }
+        }
+    }
+}
